a98fdac4605c3aebb422685c277b0fa8dae2a3fe
David Chicas <dchicasduena@mun.ca>
2021-12-06 21:49:13 -0330

    day six

--- day six/day6.py
+++ day six/day6.py
@@ -1,6 +1,33 @@
-if __name__ == "__main__":
-    input = open('input.txt','r')
-    print("\nadvent of code day 6\n")
+def reproduce(input, part):
+    partS = ""
+    if part == 80:
+        partS = "part 1 = "
+    elif part == 256:
+        partS = "part 2 = "
+    
+    fish = [int(i) for i in input]
+    days = [0,0,0,0,0,0,0,0,0]
+    
+    for f in fish:
+        days[f] += 1
+    
+    for d in range(part):
+        day = days[0]
+        for i in range(len(days) - 1):
+            days[i] = days[i + 1]
+        days[8] = day
+        days[6] += day    
+    print(partS,sum(days))
+    print("")
+
+print("\nadvent of code day 6\n")
+input = open('input.txt','r').read().split(',')
+    
+reproduce(input, 80)
+reproduce(input, 256)
+
+
+
 
 
     
